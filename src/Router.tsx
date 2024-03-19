114f91a7f0432229852674f5c668066f6fb5526d
floodbost <iwaninfo@gmail.com>
2024-03-19 14:34:35 +0700

    update route

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,10 @@ import { HomePage } from './pages/Home.page';
 
 const router = createBrowserRouter([
   {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
     path: '/webphone-sdk-demo',
     element: <HomePage />,
   },
